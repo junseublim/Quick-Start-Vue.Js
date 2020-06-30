20e863bdcdbb4f69bfab2633385cb1b5a90baaf4
Jun <junslim11@gmail.com>
2020-06-30 14:57:04 +0900

    chapter 9 slot

---
+++ Chapter9/slottest/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <speech-box :headerText="A.header" :footerText="A.footer">
+      <div>
+        <p>
+          {{A.message}}
+        </p>
+      </div>
+    </speech-box>
+
+    <speech-box class="sanders" :headerText="B.header" :footerText="B.footer">
+      <div>
+        <p class = "sanders-content">
+          {{B.message}}
+        </p>
+      </div>
+    </speech-box>
+  </div>
+</template>
+
+<script>
+import SpeechBox from './components/SpeechBox.vue'
+export default {
+  name: 'app',
+  components: {SpeechBox},
+  data : function() {
+    return {
+      A: {
+        header: "오바마",
+        footer: "20190203",
+        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      },
+      B: {
+        header: "샌더스",
+        footer: "20120212",
+        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."  
+      }
+    };
+  }
+}
+</script>
+
+<style scoped>
+.sanders {background-color: antiquewhite;}
+.sanders-content {font-family: 굴림; text-decoration: underline;}
+</style>
